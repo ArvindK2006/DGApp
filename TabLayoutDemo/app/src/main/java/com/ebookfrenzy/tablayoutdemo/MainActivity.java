fbd3612c93e510e816985babb0aca2a2f8a2e172
ArvindK2006 <kakanavaram9172@students.d211.org>
2023-03-07 08:20:03 -0600

    march 7 2023
    replicated home screen, removed mail icon at bottom right

--- TabLayoutDemo/app/src/main/java/com/ebookfrenzy/tablayoutdemo/MainActivity.java
+++ TabLayoutDemo/app/src/main/java/com/ebookfrenzy/tablayoutdemo/MainActivity.java
@@ -35,14 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         configureTabLayout();
     }
 
