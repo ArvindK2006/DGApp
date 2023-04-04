97752c32755526fb3354410aebdf9110d829188c
ArvindK2006 <90778173+ArvindK2006@users.noreply.github.com>
2023-04-04 08:03:21 -0500

    april 4 2023
    talk to denna about button in fragment

--- TabLayoutDemo/app/src/main/java/com/ebookfrenzy/tablayoutdemo/SignUpFragment.java
+++ TabLayoutDemo/app/src/main/java/com/ebookfrenzy/tablayoutdemo/SignUpFragment.java
@@ -6,8 +6,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +38,10 @@ public class SignUpFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+  /* Session session;
+    Properties props;
+    final String username = "raghus06@yahoo.com";*/
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -57,10 +72,103 @@ public class SignUpFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
 
-            //txtEmail = findViewById(R.id.txtEmail);
-            //txtMessage = findViewById(R.id.txtMessage);
-            //btnSend = findViewById(R.id.btnSend);
+            txtEmail = txtEmail.findViewById(R.id.txtEmail);
+            txtMessage = txtMessage.findViewById(R.id.txtMessage);
+            btnSend = btnSend.findViewById(R.id.btnSend);
+
+
+           // final String username = "raghus06@yahoo.com";
+
+         /*   final String password = "eqqkkevzohdkbaon";
+            props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            //props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.ssl.enable", "true");
+            props.put("mail.smtp.host", "smtp.mail.yahoo.com");
+            props.put("mail.smtp.port", "465");
+
+            session = Session.getInstance(props, new javax.mail.Authenticator(){
+                protected PasswordAuthentication getPasswordAuthentication(){
+                    return new PasswordAuthentication(username, password);
+                }
+            });*/
+
+
+
+            /* btnSend.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    final String username = "raghus06@yahoo.com";
+                    final String password = "eqqkkevzohdkbaon";
+                    String messageToSend = txtMessage.getText().toString();
+                    Properties props = new Properties();
+                    props.put("mail.smtp.auth", "true");
+                    //props.put("mail.smtp.starttls.enable", "true");
+                    props.put("mail.smtp.ssl.enable", "true");
+                    props.put("mail.smtp.host", "smtp.mail.yahoo.com");
+                    props.put("mail.smtp.port", "465");
+
+                     Session session = Session.getInstance(props, new javax.mail.Authenticator(){
+                        protected PasswordAuthentication getPasswordAuthentication(){
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+                    try{
+                        Message message = new MimeMessage(session);
+                        message.setFrom(new InternetAddress(username));
+                        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(txtEmail.getText().toString()));
+                        message.setSubject("Sending email without opening gmail app");
+                        message.setText(messageToSend);
+                        Transport.send(message);
+                      //  Toast.makeText(getApplicationContext(), "email sent successfully", Toast.LENGTH_LONG).show();
+
+                    } catch (MessagingException e){
+                        throw new RuntimeException(e);
+                    }
+                }
+            }); */
         }
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        //this.fragment = view; look for fragmentview
+
+        int idView = getResources().getIdentifier("btnSend", "id", getContext().getPackageName());
+        View eventView = view.findViewById(idView);
+        eventView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                final String username = "raghus06@yahoo.com";
+                final String password = "eqqkkevzohdkbaon";
+                String messageToSend = txtMessage.getText().toString();
+                Properties props = new Properties();
+                props.put("mail.smtp.auth", "true");
+                //props.put("mail.smtp.starttls.enable", "true");
+                props.put("mail.smtp.ssl.enable", "true");
+                props.put("mail.smtp.host", "smtp.mail.yahoo.com");
+                props.put("mail.smtp.port", "465");
+
+                 Session session = Session.getInstance(props, new javax.mail.Authenticator(){
+                    protected PasswordAuthentication getPasswordAuthentication(){
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+                try{
+                    Message message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress(username));
+                    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(txtEmail.getText().toString()));
+                    message.setSubject("Sending email without opening gmail app");
+                    //message.setText(messageToSend);
+                    message.setText("android test");
+                    Transport.send(message);
+                    //  Toast.makeText(getApplicationContext(), "email sent successfully", Toast.LENGTH_LONG).show();
+
+                } catch (MessagingException e){
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     @Override
