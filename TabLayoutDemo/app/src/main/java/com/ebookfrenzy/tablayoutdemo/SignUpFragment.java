7c5ae41028eca96fe414c936e593a89d31b7550a
ArvindK2006 <90778173+ArvindK2006@users.noreply.github.com>
2023-05-03 00:15:05 -0500

    Revert "may 2 2023"
    This reverts commit 092d234c853f795d12146fcd3b4f2650207c67f9.

--- TabLayoutDemo/app/src/main/java/com/ebookfrenzy/tablayoutdemo/SignUpFragment.java
+++ TabLayoutDemo/app/src/main/java/com/ebookfrenzy/tablayoutdemo/SignUpFragment.java
@@ -117,12 +117,12 @@ public class SignUpFragment extends Fragment {
                 fr.addToBackStack(null);
                 fr.commit();
 
-                /*Tab3Fragment frg = null;
+                /* Tab3Fragment frg = null;
                 frg = (Tab3Fragment) getFragmentManager().findFragmentByTag("fragment_tab3");
                 final FragmentTransaction ft = getFragmentManager().beginTransaction();
                 ft.detach(frg);
                 ft.attach(frg);
-                ft.commit();*/ //Crashing app -- continue trying with this
+                ft.commit(); Crashing app -- continue trying with this */
 
             }
         });
